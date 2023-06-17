cf81a00c34a9c8c844e646f1f3933d8772e2961d
Eometheous <jonathan.sthomas@icloud.com>
2023-06-17 14:34:03 -0700

    Added Gradle for building the project

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.mysql:mysql-connector-j:8.0.32")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
